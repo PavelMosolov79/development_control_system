121f424a1e25aff482cda4d890040aa34200afae
JaneBragina <Pavel.Mosolov.02@mail.ru>
2024-04-29 07:43:40 +0300

    The project has been assembled
    The basic functionality of the server part is implemented on Node.js and Express and frontend on React.js

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,36 @@
+const db = require('../db')
+
+class UserController {
+    async createUser(req, res) {
+        const {name, surname, middleName, password, email, phone} = req.body
+        const newPerson = await db.query('INSERT INTO person (name, surname, middleName, password, email, phone)' +
+                                         'values ($1, $2, $3, $4, $5, $6) RETURNING *', [name, surname, middleName, password, email, phone])
+        res.json(newPerson.rows[0])
+    }
+
+    async getUser(req, res) {
+        const users = await db.query('SELECT * FROM person')
+        res.json(users.rows)
+    }
+
+    async getOneUser(req, res) {
+        const id = req.params.id
+        const user = await db.query('SELECT * FROM person where id = $1', [id])
+        res.json(user.rows[0])
+    }
+
+    async updateUser(req, res) {
+        const {id, name, surname, middleName, password, email, phone} = req.body
+        const user = await db.query("UPDATE person set name = $2, surname = $3, middleName = $4, password = $5, email = $6, phone = $7 " +
+            "where id = $1  RETURNING *", [id, name, surname, middleName, password, email, phone])
+        res.json(user.rows[0])
+    }
+
+    async deleteUser(req, res) {
+        const id = req.params.id
+        const user = await db.query('DELETE FROM person where id = $1', [id])
+        res.json(user.rows[0])
+    }
+}
+
+module.exports = new UserController();
---
+++ backend/database.sql
@@ -0,0 +1,9 @@
+create TABLE person(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    middleName VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255)
+);
